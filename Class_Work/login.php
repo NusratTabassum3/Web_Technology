18280e729ff38f07b8bccd475764a3b42511de57
Nusrat Tabassum <tabassumnusrat3@gmail.com>
2023-02-13 21:02:17 +0600

    Create login.php

---
+++ Class_Work/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head><h1> <meta charset="utf-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> LOGIN Page</title></h1>
+
+
+
+
+
+
+</head>
+
+<body>
+    <h1 align=center><b> Login Page </b> </h1>
+    <br><br>
+    
+    <table align='center'>
+        <td>
+            <fieldset>
+                <legend >Login :</legend>
+                <form action="/action_page.php" novalidate></form>
+
+
+
+
+
+                <label for="uname">Username:</label>
+                <input type="text" id="uname" name="uname"><br> <br>
+                <label for="pass">Password:</label>
+                <input type="pass" id="pass" name="pass"><br> <br>
+                <p align="right"><input type="submit" value="LogIn" name='submit'></p>
+                
+                </fieldset >
+                
+              
+            
+            
+            
+            
+            
+          
+
+            <p align="center">Don't have an account?<a href="registration.php">Registration Here</a></p>
+        </body>
+        </html>
