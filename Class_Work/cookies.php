3271811d0d0611a0c18d0cac52602734e9d3710c
Nusrat Tabassum <tabassumnusrat3@gmail.com>
2023-02-18 17:07:18 +0600

    Add files via upload

---
+++ Class_Work/cookies.php
@@ -0,0 +1,69 @@
+<?php
+    if(isset($_POST['btn'])){
+        $color= $_POST['color'];
+        $date= $_POST['date'];
+    
+        $error = [];
+        if(empty($_POST['color'])){
+            $error['color']='Select Color';
+        }
+        if(empty($_POST['date'])){
+            $error['date']='Select Date';
+        }
+
+    }
+    if(isset($_POST['bttn'])){
+        // if(empty($_POST['date'])){
+        //     $error['date']='Select Date';
+        // }
+        
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cookies</title>
+</head>
+<body>
+
+	<h1>Cookies Example</h1>
+    <p1>Time zone: Asia </p1>
+    <hr>
+    <h3>Set Cookies</h3>
+    <hr>
+	<form action="" method="POST">
+        <label for="colour">Select a color: </label>
+		<input type="color" name="color" value="#000000"><br>
+        <span>
+        <?php
+          if(isset($error['color'])){
+            echo $error['color'];
+          }
+        ?><br>
+        </span>
+
+
+        <label for="" > Expire on </label>
+        <input type="datetime-local" name="date"> <br>
+        <span>
+        <?php
+          if(isset($error['date'])){
+            echo $error['date'];
+          }
+        ?><br><br>
+        </span>
+
+
+		<input type="submit" name = "btn" value="Set Cookies"|>
+	</form>
+    <hr>
+    <h3>Set Cookies</h3>
+    <hr>
+    <form action="" method="POST">
+    <input type="submit" name = "bttn" value="Destroy Cookies"|>
+	</form>
+
+</body>
+</html>
